17c7531c9a8a64972635594e56fe7b25546f8eff
VovJur <56715595+VovJur@users.noreply.github.com>
2021-06-02 14:19:05 +0200

    Update media.js

--- media.js
+++ media.js
@@ -121,3 +121,57 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+//Pictures zoom on click
+var modal = document.getElementById("myModal");
+
+var img = document.getElementById("pic_zoom7");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+//Pictures zoom on click
+var modal = document.getElementById("myModal");
+
+var img = document.getElementById("pic_zoom8");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+//Pictures zoom on click
+var modal = document.getElementById("myModal");
+
+var img = document.getElementById("pic_zoom9");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
